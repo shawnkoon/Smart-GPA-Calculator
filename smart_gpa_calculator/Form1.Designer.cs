c02ab4c12f05da489a67c7a448bce471129e4e32
shawnkoon <shawn9405@gmail.com>
2016-11-10 23:51:50 -0800

    Added basic layouts to the main Form

--- smart_gpa_calculator/Form1.Designer.cs
+++ smart_gpa_calculator/Form1.Designer.cs
@@ -28,21 +28,109 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.YellowGreen;
+            this.label1.Location = new System.Drawing.Point(39, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(125, 49);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Smart";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Comic Sans MS", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.LightCoral;
+            this.label2.Location = new System.Drawing.Point(129, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 49);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "GPA";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Comic Sans MS", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label3.Location = new System.Drawing.Point(216, 26);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(260, 67);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Calculator";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.button1.Font = new System.Drawing.Font("Comic Sans MS", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(71, 157);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(352, 90);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Cumulative GPA";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.LightCoral;
+            this.button2.Font = new System.Drawing.Font("Comic Sans MS", 20.25F, System.Drawing.FontStyle.Bold);
+            this.button2.Location = new System.Drawing.Point(71, 258);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(352, 90);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Single Class GPA";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.LightGreen;
+            this.button3.Font = new System.Drawing.Font("Comic Sans MS", 20.25F, System.Drawing.FontStyle.Bold);
+            this.button3.Location = new System.Drawing.Point(71, 359);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(352, 90);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "About";
+            this.button3.UseVisualStyleBackColor = false;
             // 
             // mainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(539, 464);
+            this.BackColor = System.Drawing.Color.LemonChiffon;
+            this.ClientSize = new System.Drawing.Size(488, 464);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
             this.Name = "mainForm";
             this.Text = "Smart GPA Calculator";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
